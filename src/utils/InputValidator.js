a8775b83b834f9f0d7035d805b5e7151eb82f3cd
Yusril A. P <yusriladr.37@gmail.com>
2022-04-29 22:16:13 +0700

    Fix password minimum length bug

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -11,7 +11,7 @@ const InputValidator = {
   validateSignUp: ({ username, password, confirmPassword }) => {
     if (username.trim().includes(' ')) throw new InvariantError('Username should not contain any spaces');
 
-    if (password.length > CONFIG.USER_PASSWORD_MIN_LENGTH) throw new InvariantError(`Password length Minimum is ${CONFIG.USER_PASSWORD_MIN_LENGTH}`);
+    if (password.length < CONFIG.USER_PASSWORD_MIN_LENGTH) throw new InvariantError(`Password length Minimum is ${CONFIG.USER_PASSWORD_MIN_LENGTH}`);
 
     if (password !== confirmPassword) throw new InvariantError('Confirm password is not valid.');
   },
